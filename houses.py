20c8f59b7c918e570b03f30447cb337f2997347a
Oscar Nilsson <oscarandersnilsson@gmail.com>
2021-10-11 21:33:12 +0200

    adding houses

---
+++ houses.py
@@ -0,0 +1,27 @@
+house1 = {
+    "price": 100,
+    "rent": 50,
+    "description": "a yellow house",
+    "owner": "the bank"
+}
+
+house2 = {
+    "price": 200,
+    "rent": 100,
+    "description": "a red house",
+    "owner": "the bank"
+}
+
+house3 = {
+    "price": 300,
+    "rent": 150,
+    "description": "a green house",
+    "owner": "the bank"
+}
+
+house4 = {
+    "price": 400,
+    "rent": 200,
+    "description": "a blue house",
+    "owner": "the bank"
+}
