59619e6bfc76f329969fd13210e34dbe2d040eb3
Oscar Nilsson <oscarandersnilsson@gmail.com>
2021-11-02 14:02:11 +0100

    changed houses information

--- houses.py
+++ houses.py
@@ -1,30 +1,22 @@
 houses = [
     {
-        "id": "id1",
         "price": 100,
         "rent": 50,
         "description": "a yellow house",
-        "actions": ["buy", "sell"]
     },
     {
-        "id": "id2",
         "price": 200,
         "rent": 100,
         "description": "a purple house",
-        "actions": ["buy", "sell"]
     },
     {
-        "id": "id3",
         "price": 300,
         "rent": 150,
         "description": "a green house",
-        "actions": ["buy", "sell"]
     },
     {
-        "id": "id4",
         "price": 400,
         "rent": 200,
         "description": "a black house",
-        "actions": ["buy", "sell"]
     }
 ]
